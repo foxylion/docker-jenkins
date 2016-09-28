10508265e792fbea58fb2bed076dedad1a916507
itd: Jakob Jarosch <jj@itdesign.de>
2016-09-28 10:35:31 +0200

    Add support for JENKINS_USER and JENKINS_PASSWORD env vars
    Customize login credentials of the Jenkins master at initial startup

--- docker-images/master/default-user.groovy
+++ docker-images/master/default-user.groovy
@@ -1,11 +1,13 @@
 import jenkins.model.*
 import hudson.security.*
 
+def env = System.getenv()
+
 def jenkins = Jenkins.getInstance()
 jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
 jenkins.setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy())
 
-def user = jenkins.getSecurityRealm().createAccount('admin', 'admin')
+def user = jenkins.getSecurityRealm().createAccount(env.JENKINS_USER, env.JENKINS_PASSWORD)
 user.save()
 
 jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, 'admin')
