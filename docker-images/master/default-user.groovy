2a237c26f9498ad2e8151408ba23b46711d66f12
itd: Jakob Jarosch <jj@itdesign.de>
2016-09-28 15:34:10 +0200

    Fix usage of wrong env variable in default-user.groovy

--- docker-images/master/default-user.groovy
+++ docker-images/master/default-user.groovy
@@ -7,7 +7,7 @@ def jenkins = Jenkins.getInstance()
 jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
 jenkins.setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy())
 
-def user = jenkins.getSecurityRealm().createAccount(env.JENKINS_USER, env.JENKINS_PASSWORD)
+def user = jenkins.getSecurityRealm().createAccount(env.JENKINS_USER, env.JENKINS_PASS)
 user.save()
 
 jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, 'admin')
