e8204fdbf43020e99510b5d7d4a9980ce37219a7
Jakob Jarosch <foxylion@users.noreply.github.com>
2019-03-07 09:36:41 +0100

    Do not override permission scheme
    In the current state the permission scheme was updated
    on every container restart, this prevented the user
    from adding users with custom permissions.
    
    From now on you can use the matrix based permissions
    and they will persist between restarts of the jenkins
    container.
    
    #7

--- docker-images/master/default-user.groovy
+++ docker-images/master/default-user.groovy
@@ -4,11 +4,14 @@ import hudson.security.*
 def env = System.getenv()
 
 def jenkins = Jenkins.getInstance()
-jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
-jenkins.setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy())
+if(!(jenkins.getSecurityRealm() instanceof HudsonPrivateSecurityRealm))
+    jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
+
+if(!(jenkins.getAuthorizationStrategy() instanceof GlobalMatrixAuthorizationStrategy))
+    jenkins.setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy())
 
 def user = jenkins.getSecurityRealm().createAccount(env.JENKINS_USER, env.JENKINS_PASS)
 user.save()
-
 jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, env.JENKINS_USER)
+
 jenkins.save()
